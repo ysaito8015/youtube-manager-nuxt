a4d8c7fa2912fc704a2fa2913b395535b7510188
Yusuke_Saito <ysaito8015@gmail.com>
2022-09-09 16:40:59 +0200

    done 4.3.7

---
+++ components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="navbar">
+		<div class="navbar-brand">
+			<nuxt-link
+				class="navbar-item subtitle is-4"
+				to="/"
+			>
+				Youtube Player
+			</nuxt-link>
+		</div>
+	</nav>
+</template>
---
+++ components/TheSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="columsn is-2">
+    <div class="section">
+      <aside class="menu">
+        <ul class="menu-list">
+          <li>
+            <nuxt-link
+							to="/"
+						>
+							ホーム
+						</nuxt-link>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  </div>
+</template>
