2c463c1711917ed8e65977fcba581f159dd729ac
Yusuke_Saito <ysaito8015@gmail.com>
2023-01-01 20:36:37 +0900

    Add search box on the header

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -7,6 +7,23 @@
       >
         Youtube Player
       </nuxt-link>
-    </div>
+    </div> <!-- end of navbar-brand -->
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <div class="field has-addons">
+            <div class="control">
+              <input name="q" class="input" type="text" placeholder="検索">
+            </div> <!-- end of control -->
+            <div class="control">
+              <button class="button">
+                Search
+              </button>
+            </div> <!-- end of control -->
+          </div> <!-- end of field has-addons -->
+        </div> <!-- end of navbar-item -->
+      </div> <!-- end of navbar-start -->
+    </div> <!-- end of navbar-menu -->
   </nav>
 </template>
