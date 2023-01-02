873541a7f61bfaa97d689447f65c30d93405df99
Yusuke Saito <ysaito8015@gmail.com>
2023-01-02 15:04:15 +0900

    Finish Ch.06 (#4)
    * Add search box on the header
    
    * Add search page
    
    * Add a route to search api
    
    * Add store functions
    
    * Fix missing tags and typo

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -7,6 +7,27 @@
       >
         Youtube Player
       </nuxt-link>
-    </div>
+    </div> <!-- end of navbar-brand -->
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <div class="field has-addons">
+            <form action = "/search" method = "get">
+              <div class="field has-addons">
+                <div class="control">
+                  <input name="q" class="input" type="text" placeholder="検索">
+                </div> <!-- end of control -->
+                <div class="control">
+                  <button class="button">
+                    Search
+                  </button>
+                </div> <!-- end of control -->
+              </div> <!-- end of field has-addons -->
+            </form> <!-- end of search action form -->
+          </div> <!-- end of field has-addons -->
+        </div> <!-- end of navbar-item -->
+      </div> <!-- end of navbar-start -->
+    </div> <!-- end of navbar-menu -->
   </nav>
 </template>
