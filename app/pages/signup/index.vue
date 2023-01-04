a035ecf1fb902d107592d44b46372d59b53e1ff8
Yusuke_Saito <ysaito8015@gmail.com>
2023-01-04 11:24:26 +0900

    Add sign up page

---
+++ app/pages/signup/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="section">
+    <div class="columns">
+      <div class="column is-6">
+        <div class="box">
+          <div class="field">
+            <label class="label" for="">Email</label>
+            <div class="control">
+              <input
+                v-model = "email"
+                class = "input is-info"
+                type = "email"
+                placeholder = "Email"
+              >
+            </div> <!-- end of control -->
+          </div> <!-- end of field -->
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input
+                v-model = "pasword"
+                class = "input is-info"
+                type = "password"
+                placeholder = "Password"
+              >
+            </div> <!-- end of control -->
+          </div> <!-- end of field -->
+        </div> <!-- end of box -->
+
+        <div class="field is-grouped">
+          <div class="control">
+            <button
+              class = "button is-link"
+              @click = "signUp"
+              >Sign up</button>
+          </div> <!-- end of control -->
+        </div> <!-- end of field -->
+      </div> <!-- end of column is-6 -->
+    </div> <!-- end of columns -->
+  </div> <!-- end of section -->
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: "",
+        password: ""
+      }
+    },
+
+    methods: {
+      signUP() {
+        let user = {
+          email: this.email,
+          password: this.password
+        }
+        this.$store.dispatch('signUp', user)
+      }
+    }
+  }
+
+</script>
